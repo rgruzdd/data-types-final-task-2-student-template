5812854c78490dd0879c9ba5b87418377f90a14e
Gruzin <gruzd.roman@inbox.ru>
2023-03-15 20:05:24 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -4,4 +4,16 @@ def check(row_start:int, row_end:int, column_start:int, column_end:int) -> List[
     """
     Add your code here or call it from here   
     """
+    new_list = []
+    for i in range(row_start, row_end+1):
+        n_new = []
+        for k in range(column_start, column_end+1):
+            n_new.append(i*k)
+        new_list.append(n_new)
+
+    return new_list
+
+
     pass
+
+
